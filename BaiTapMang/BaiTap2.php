a74ec16b4d8a963a74d676e8d25a838d7289278e
unknown <nguyentanhaipccc@gmail.com>
2020-07-05 17:31:47 +0700

    Changed file BaiTapMang/BaiTap2.php

---
+++ BaiTapMang/BaiTap2.php
@@ -0,0 +1,21 @@
+<?php
+//Viet chuong trinh tao mang lien hop hiển thị tên quốc gia và thủ đô<f>x </f>
+
+$ArrayCountry = array("Italia"=>"Rome", "Ireland"=>"Dublin", "Australia"=>"Vienna");
+
+foreach($ArrayCountry as $Country=>$Capital){
+    echo "Thủ đô của $Country là $Capital"."<br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
+
