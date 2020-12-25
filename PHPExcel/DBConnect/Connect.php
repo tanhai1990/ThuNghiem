c571a9532f5ae83649c5e44d0b2e75909e52bee6
unknown <nguyentanhaipccc@gmail.com>
2020-12-25 16:53:20 +0700

    Add new file

--- PHPExcel/DBConnect/Connect.php
+++ PHPExcel/DBConnect/Connect.php
@@ -1,24 +1,14 @@
 <?php
+$ServerName = "localhost";
+$UserName = "root";
+$PassWord = "";
+$DBName = "dbexcel";
 
-$mysqli = mysqli_connect('localhost','root','','exceldemodb');
-$mysqli->set_charset('utf8');
-if(mysqli_connect_error()){
-    echo "Connect failed: ".mysqli_connect_error();
-    exit();
+$Connect = mysqli_connect($ServerName, $UserName, $PassWord, $DBName);
+
+if(!$Connect){
+    die("Connection failed: ".mysqli_connect_error());
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 ?>
--- PHPExcel/index.php
+++ PHPExcel/index.php
@@ -1,36 +1,17 @@
 <?php
-require "./DBConnect/Connect.php";
-require "./Classes/PHPExcel.php";
+require "DBConnect/Connect.php";
+require "Classes/PHPExcel.php";
 
 if(isset($_POST["btnGui"])){
     $file = $_FILES['file']['tmp_name'];
-    
-    $objReader = PHPExcel_IOFactory::createReaderForFile($file);
+
+    $ObjReader = PHPExcel_IOFactory::createReaderForFile($file);
+
     $objReader->setLoadSheetsOnly('10A1');
-
-    $objExcel = $objReader->load($file);
-    $sheetData = $objExcel->getActiveSheet()->toArray('null',true,true,true);
-    print_r($sheetData);
-
-    $HighestRow = $objExcel->setActiveSheetIndex()->getHighestRow();
     
-    for($row=2;$row<=$HighestRow;$row++){
-        $IDLop = 1;
-        $name = $sheetData[$row]['A'];
-        $DiemToan = $sheetData[$row]['B'];
-        $DiemLy = $sheetData[$row]['C'];
-        $DiemHoa = $sheetData[$row]['D'];
-
-        $sql = "
-            INSERT INTO diem(IDLop, TenHocVien, DiemToan, DiemLy, DiemHoa)
-            VALUES($IDLop, '$name', $DiemToan, $DiemLy, $DiemHoa);
-        ";
-        $mysqli->query($sql);
-    }
-    echo 'Inserted';
 
 
-
+    
 }
 ?>
 
